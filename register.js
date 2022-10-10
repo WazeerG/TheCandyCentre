28fe9964127d6d91fca7c6a53d999fb49f740c8a
WazeerG <109173867+WazeerG@users.noreply.github.com>
2022-10-10 14:11:28 +0200

    Update register.js

--- register.js
+++ register.js
@@ -63,7 +63,7 @@ document.getElementById("register-btn").addEventListener('click', function() {
       const user = userCredential.user;
       alert("Success");
 
-      function(register-btn) {
+      function("register-btn") {
         "window.location.href='products.html';"
       }
       // ...
